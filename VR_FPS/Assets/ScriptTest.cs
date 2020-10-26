f34f07bd3b36b644eb5618cc3a6709b64c2a786f
Chris Christensen <chriscrossed.86@gmail.com>
2020-10-25 21:58:41 -0700

    Basic gunfire visuals

--- VR_FPS/Assets/ScriptTest.cs
+++ VR_FPS/Assets/ScriptTest.cs
@@ -10,34 +10,52 @@ public class ScriptTest : MonoBehaviour
     public SteamVR_Input_Sources thisHand;
     public GameObject handModel;
 
+    private Transform thisFireball;
+    private float fireballTimer;
+    public float fireballTimerMax = 0.1f;
+    private bool fireGun;
+    private bool triggerPressed;
+
     // Start is called before the first frame update
     void Start()
     {
         Trigger.AddOnStateDownListener(TriggerDown, thisHand);
         Trigger.AddOnStateUpListener(TriggerUp, thisHand);
+
+        thisFireball = handModel.transform.Find("Fireball");
     }
 
     public void TriggerUp(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
     {
         Debug.Log("Trigger is Up");
-        handModel.GetComponent<MeshRenderer>().enabled = true;
-
-        foreach(MeshRenderer childMesh in handModel.transform.GetComponentsInChildren<MeshRenderer>())
-            childMesh.enabled = true;
+        triggerPressed = false;
     }
 
     public void TriggerDown(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
     {
         Debug.Log("Trigger is Down");
-        handModel.GetComponent<MeshRenderer>().enabled = false;
-
-        foreach (MeshRenderer childMesh in handModel.transform.GetComponentsInChildren<MeshRenderer>())
-            childMesh.enabled = false;
+        if( !triggerPressed ) fireGun = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (fireGun)
+        {
+            fireGun = false;
+            fireballTimer = fireballTimerMax;
+
+            thisFireball.GetComponent<MeshRenderer>().enabled = true;
+        }
+
+        if(fireballTimer > 0f)
+        {
+            fireballTimer -= Time.deltaTime;
+            if (fireballTimer <= 0f)
+            {
+                fireballTimer = 0f;
+                thisFireball.GetComponent<MeshRenderer>().enabled = false;
+            }
+        }
     }
 }
